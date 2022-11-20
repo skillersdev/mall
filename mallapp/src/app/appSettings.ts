eec29c0703bebed022e192de52e1af3991bf4bae
sridhar <sridhar.muthusamy@signzy.com>
2022-11-20 23:51:35 +0530

    base url

--- mallapp/src/app/appSettings.ts
+++ mallapp/src/app/appSettings.ts
@@ -2,8 +2,8 @@ export class AppSettings{
 
 	
 	
-	 public static API_BASE = "http://localhost/ag7dev/trunk/api";	
-	 public static IMAGE_BASE = "http://localhost/ag7dev/trunk/api/mallproduct_image/";
+	 public static API_BASE = "http://localhost/3abc7dev/ag7dev/api";	
+	 public static IMAGE_BASE = "http://localhost/3abc7dev/ag7dev/api/mallproduct_image/";
 	//  public static IMAGE_BASE = "https://roodabatoz.com/api/mallproduct_image/";
 
 	public static getmallproductDetail = AppSettings.API_BASE + "/getmallproduct"; 
